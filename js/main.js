22cad9f65cfc9700cd812c8587d38ad106ad0ced
Rinor Agaj <rinoragaj@noreply.github.com>
2023-12-08 21:42:07 +0100

    Fourth Commit - Rinori
    1. Eshte krijuar log-in forma edhe register forma, jane lidhur me butonin account
    
    2. Eshte punuar ne index.html ne pjesen <main> (image slideshow)
    
    3. Eshte perfunduar komplet pjesa <main> e Reservation page, bashk me nje "Book Now" button i cili e shfaq nje popup reservation form
    
    4. Eshte punuar ne pjesen e header ne index.html , about.html, menu.html dhe reservation.html - bug fixes ne javascript.
    
    5. Jan shtuar files te ndryshme ne "img" folder:
    
    6. Eshte punuar ne style.css (multiple bug fixes)
    
    7. Ndryshime te ndryshme te kodit dhe te funksioneve ne main.js (multiple bug fixes)
    3. Jan shtuar files te ndryshme ne "img" folder:
    
    3. Eshte ndryshuar ikoni i "cart" me ikonin "person" qe ne te ardhmen te jete funksional sistemi i login.
    
    4. Eshte shtuar nje sliding panel, i cili kur te klikohet butoni "three dots - more" shfaqet dhe tregon me shume per Bar-in (about us)

--- js/main.js
+++ js/main.js
@@ -1,38 +1,130 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const header = document.querySelector('.header');
+    const contentBelowHeader = document.querySelector('.content-below-header');
+    let isScrolling = false;
 
-    window.addEventListener('scroll', function() {
+    // Funksioni qe e bon update padding te klases .content-below-header
+    function updateContentPadding() {
+        const newPaddingTop = header.offsetHeight;
+
+        
         if (window.scrollY > 50 && !header.classList.contains('header-smaller')) {
             header.classList.add('header-smaller');
         } else if (window.scrollY <= 50 && header.classList.contains('header-smaller')) {
             header.classList.remove('header-smaller');
         }
+
+        // Vendoset padding-top i .content-below-header duhet mar parasysh "height" te header
+        contentBelowHeader.style.paddingTop = `${newPaddingTop}px`;
+    }
+
+    // Thirret funksioni kur faqja behet reload ose kur faqja (window) behet "resized".
+    window.addEventListener('load', updateContentPadding);
+    window.addEventListener('resize', updateContentPadding);
+
+    // Event listener per scroll event tu e perdor requestAnimationFrame = testing phase
+    window.addEventListener('scroll', function () {
+        if (!isScrolling) {
+            window.requestAnimationFrame(function () {
+                updateContentPadding();
+                isScrolling = false;
+            });
+            isScrolling = true;
+        }
+    });
+
+});
+
+
+
+
+    // Test per slide
+const moreButton = document.getElementById('more-button');
+const morePanel = document.getElementById('more-panel');
+const closeButton = document.getElementById('close-button');
+
+moreButton.addEventListener('click', function () {
+    morePanel.style.right = '0';
+});
+
+closeButton.addEventListener('click', function () {
+    morePanel.style.right = '-350px';
+});
+
+// Test per scroll on click
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({ behavior: 'smooth' });
     });
 });
 
-// Test per slide
-document.addEventListener('DOMContentLoaded', function () {
-    const moreButton = document.getElementById('more-button');
-    const morePanel = document.getElementById('more-panel');
-    const closeButton = document.getElementById('close-button');
+// Test per login form
+function openLoginForm() {
+    var modal = document.getElementById('loginModal');
+    modal.style.display = 'block';
+}
 
-    moreButton.addEventListener('click', function () {
-        morePanel.style.right = '0';
-    });
+function closeLoginForm() {
+    var modal = document.getElementById('loginModal');
+    modal.style.display = 'none';
+}
 
-    closeButton.addEventListener('click', function () {
-        morePanel.style.right = '-350px';
-    });
-});
+// Funksioni qe e mbyll popup-in nese useri klikon jasht formes
+window.onclick = function (event) {
+    var modal = document.getElementById('loginModal');
+    if (event.target == modal) {
+        modal.style.display = 'none';
+    }
+};
+
+// Finksioni qe e shfaq register formen
+function showRegisterForm() {
+    var loginForm = document.getElementById('loginForm');
+    var registerForm = document.getElementById('registerForm');
+
+    // Hide the login form
+    loginForm.style.display = 'none';
+
+    // Show the register form
+    registerForm.style.display = 'block';
+}
+
+// Funksioni qe e mbyll register formen edhe e shfaq login formen
+function closeRegisterForm() {
+    var loginForm = document.getElementById('loginForm');
+    var registerForm = document.getElementById('registerForm');
+
+    // Mbyllet register forma
+    registerForm.style.display = 'none';
+
+    // Shfaqet login forma
+    loginForm.style.display = 'block';
+}
+//SLIDESHOW TEST
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1
+    }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 2000); // Ndryshimi i slide 
+}
 
 
-//test per scroll on click
-document.addEventListener("DOMContentLoaded", function () {
-    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-        anchor.addEventListener('click', function (e) {
-            e.preventDefault();
+//Per reservation popup
+function toggleReservationForm() {
+    var popup = document.getElementById("reservationPopup");
+    popup.style.display = (popup.style.display === "none" || popup.style.display === "") ? "block" : "none";
+}
 
-            document.querySelector(this.getAttribute('href')).scrollIntoView({ behavior: 'smooth' });
-        });
-    });
-});
