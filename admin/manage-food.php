a1bcbe5901d5044c1ae3b3d2735b75bed457a2e9
Rinor Agaj <rinoragaj@noreply.github.com>
2024-02-02 11:29:28 +0100

    Seventh Commit - Rinori
    Pjesa e Dyte:
    
    1.Ka fillu puna ne manage food edhe order
    2.Eshte bo funksional update-passwordi
    3. Eshte bo funksional update admin te manage-admin.php

---
+++ admin/manage-food.php
@@ -0,0 +1,54 @@
+<?php include("partials/header.php");?>
+
+        <div class="main-content">
+            <div class="wrapper">
+                <h1>Manage Food</h1>
+                <br /><br />
+
+                <a href="#" class="btn-primary">Add Food</a>
+
+                <br /><br /> <br />
+
+                <table class="tbl-full">
+                    <tr>
+                        <th>S.N</th>
+                        <th>Full Name</th>
+                        <th>Username</th>
+                        <th>Actions</th>
+                    </tr>
+
+                    <tr>
+                        <td>1.</td>
+                        <td>Rinor Agaj</td>
+                        <td>agaj123</td>
+                        <td>
+                            <a href="#" class="btn-secondary">Update Admin</a>
+                            <a href="#" class="btn-danger">Delete Admin</a>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>1.</td>
+                        <td>Rinor Agaj</td>
+                        <td>agaj123</td>
+                        <td>
+                            <a href="#" class="btn-secondary">Update Admin</a>
+                            <a href="#" class="btn-danger">Delete Admin</a>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>1.</td>
+                        <td>Rinor Agaj</td>
+                        <td>agaj123</td>
+                        <td>
+                            <a href="#" class="btn-secondary">Update Admin</a>
+                            <a href="#" class="btn-danger">Delete Admin</a>
+                        </td>
+                    </tr>
+                </table>
+                <div class="clearfix"></div>
+            </div>
+        </div>
+
+<?php include("partials/footer.php");?>
