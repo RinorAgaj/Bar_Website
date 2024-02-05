5f628fee4d09f439b0a4ece6ca9cc95bb4f7c8d8
Rinor Agaj <rinoragaj@noreply.github.com>
2024-02-05 15:47:51 +0100

    Eighth Commit - Rinori
    1.Eshte bere funksional pjesa e menaxhimit te ushqimit 'manage-food.php'.
    2.Gjitheashtu eshte bere funksional shtuarja e ushqimeve ne databaz dhe ndryshimi i te dhenave po ashtu edhe fshirja nga databaza.

--- admin/manage-food.php
+++ admin/manage-food.php
@@ -5,47 +5,100 @@
                 <h1>Manage Food</h1>
                 <br /><br />
 
-                <a href="#" class="btn-primary">Add Food</a>
+                <a href="<?php echo SITEURL; ?>admin/add-food.php" class="btn-primary">Add Food</a>
 
                 <br /><br /> <br />
+
+                <?php
+                    if(isset($_SESSION['add'])){
+                        echo $_SESSION['add'];
+                        unset($_SESSION['add']);
+                    }
+                    if(isset($_SESSION['delete'])){
+                        echo $_SESSION['delete'];
+                        unset($_SESSION['delete']);
+                    }
+                    if(isset($_SESSION['upload'])){
+                        echo $_SESSION['upload'];
+                        unset($_SESSION['upload']);
+                    }
+                    if(isset($_SESSION['unauthorize'])){
+                        echo $_SESSION['unauthorize'];
+                        unset($_SESSION['unauthorize']);
+                    }
+                    if(isset($_SESSION['update']))
+                    {
+                        echo $_SESSION['update'];
+                        unset($_SESSION['update']);
+                    }
+
+                ?>
 
                 <table class="tbl-full">
                     <tr>
                         <th>S.N</th>
-                        <th>Full Name</th>
-                        <th>Username</th>
+                        <th>Title</th>
+                        <th>Price</th>
+                        <th>Image</th>
+                        <th>Featured</th>
+                        <th>Active</th>
                         <th>Actions</th>
                     </tr>
+                    
+                    <?php
+                        $sql = "SELECT * FROM tbl_food";
+                        $res = mysqli_query($conn, $sql);
 
-                    <tr>
-                        <td>1.</td>
-                        <td>Rinor Agaj</td>
-                        <td>agaj123</td>
-                        <td>
-                            <a href="#" class="btn-secondary">Update Admin</a>
-                            <a href="#" class="btn-danger">Delete Admin</a>
-                        </td>
-                    </tr>
+                        $count = mysqli_num_rows($res);
+                        $sn=1;
 
-                    <tr>
-                        <td>1.</td>
-                        <td>Rinor Agaj</td>
-                        <td>agaj123</td>
-                        <td>
-                            <a href="#" class="btn-secondary">Update Admin</a>
-                            <a href="#" class="btn-danger">Delete Admin</a>
-                        </td>
-                    </tr>
+                        if($count>0)
+                        {
+                            while($row=mysqli_fetch_assoc($res))
+                            {
+                                $id=$row['id'];
+                                $title=$row['title'];
+                                $price=$row['price'];
+                                $image_name=$row['image_name'];
+                                $featured=$row['featured'];
+                                $active=$row['active'];
 
-                    <tr>
-                        <td>1.</td>
-                        <td>Rinor Agaj</td>
-                        <td>agaj123</td>
-                        <td>
-                            <a href="#" class="btn-secondary">Update Admin</a>
-                            <a href="#" class="btn-danger">Delete Admin</a>
-                        </td>
-                    </tr>
+                                ?>
+                                <tr>
+                                    <td><?php echo $sn++; ?></td>
+                                    <td><?php echo $title; ?></td>
+                                    <td><?php echo $price; ?>€</td>
+                                    <td>
+                                        <?php 
+                                            if($image_name=="")
+                                            {
+                                                echo "<div class='error'>Image not Added.</div>";
+                                            }
+                                            else
+                                            {
+                                                ?>
+                                                <img src="<?php echo SITEURL; ?>img/food/<?php echo $image_name;?>" width="100px">
+                                                <?php
+                                            }
+                                        ?>
+                                    </td>
+                                    <td><?php echo $featured; ?></td>
+                                    <td><?php echo $active; ?></td>
+                                    <td>
+                                        <a href="<?php echo SITEURL; ?>admin/update-food.php?id=<?php echo $id; ?>" class="btn-secondary">Update Admin</a>
+                                        <a href="<?php echo SITEURL; ?>admin/delete-food.php?id=<?php echo $id; ?>&image_name=<?php echo $image_name; ?>" class="btn-danger">Delete Admin</a>
+                                    </td>
+                                </tr>
+
+                                <?php
+                            }
+                        }
+                        else
+                        {
+                            echo "<tr> <td colspan='7' class='error'> Food not Added Yet. </td> </tr>";
+                        }
+                    ?>
+
                 </table>
                 <div class="clearfix"></div>
             </div>
